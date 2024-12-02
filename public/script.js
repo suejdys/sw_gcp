8a90562736f4113adebf325e88fab2c202b47d65
BOOK-VLGRGDAQ94\suhye <1265asha38@gmail.com>
2024-12-02 09:40:47 +0900

    로그인 api 수정

--- public/script.js
+++ public/script.js
@@ -33,9 +33,14 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
     });
-
+    if (response.ok) {
+        alert(result.message); // 성공 메시지 표시
+    } else {
+        console.log(response.status, response.statusText); // 상태 코드와 텍스트 확인
+        alert(result.message || "Login failed");
+    }
     const result = await response.json(); // 응답을 텍스트로 가져옴
-    console.log(result.message); // 그대로 알림에 띄움
+    console.log(result); // 그대로 알림에 띄움
 
     // �α��� ���� ��, welcome.html�� �����̷�Ʈ
     if (response.ok) {
