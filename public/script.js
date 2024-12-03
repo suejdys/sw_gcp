b47dc09304787630fd36ece7ccc3eb9aa129674d
BOOK-VLGRGDAQ94\suhye <1265asha38@gmail.com>
2024-12-03 13:52:55 +0900

    main 페이지 수정

--- public/script.js
+++ public/script.js
@@ -52,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 const result = await response.json();
                 if (response.ok) {
                     alert(result.message);
-                    window.location.href = '/test.html';
+                    window.location.href = '/main.html';
                 } else {
                     alert(result.message || "로그인에 실패했습니다.");
                 }
